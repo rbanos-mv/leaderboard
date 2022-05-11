c3b2cbb5dd7ae2385a5c0df49907dd874fea72ee
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-05-11 07:28:20 -0500

    Add ScoreList class with skeleton

---
+++ src/ScoreList.js
@@ -0,0 +1,26 @@
+import api from './Api.js';
+
+const gameName = 'RBA-game';
+const gameKey = 'gameKey';
+
+class ScoreList {
+  constructor(gameKey, gameName) {
+    //
+  }
+
+  createGame = async (gameName) => {
+    //
+  };
+
+  addScore = async (user, score) => {
+    //
+  };
+
+  getScores = async () => {
+    //
+  };
+}
+
+const scoreList = new ScoreList();
+
+exports.scoreList = scoreList;
