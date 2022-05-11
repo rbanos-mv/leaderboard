14ae59e38a59df158ae121bb356ca9767ae9abd2
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-05-11 07:49:31 -0500

    Add ScoreList constructor implementation

--- src/ScoreList.js
+++ src/ScoreList.js
@@ -1,14 +1,19 @@
 import api from './Api.js';
 
-const gameName = 'RBA-game';
 const gameKey = 'gameKey';
+const gameName = 'RBA-game';
 
 class ScoreList {
-  constructor(gameKey, gameName) {
-    //
+  constructor() {
+    this.gameKey = gameKey;
+    this.gameName = gameName;
+    this.gameId = localStorage.getItem(this.gameKey);
+    if (this.gameId == null) {
+      this.createGame();
+    }
   }
 
-  createGame = async (gameName) => {
+  createGame = async () => {
     //
   };
 
